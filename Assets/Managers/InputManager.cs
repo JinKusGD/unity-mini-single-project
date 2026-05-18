f236ba91756e05d1ff2c8938f95aa7f0099088fa
Jinkus <gdjinkus@gmail.com>
2026-05-18 18:38:30 +0900

    feat: [인풋 매니저] 스크립트 추가

---
+++ Assets/Managers/InputManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    [SerializeField] private InputActionReference PlayerMoveAction;
+
+    private Action<InputAction.CallbackContext> _bindedPlayerMoveEvent;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        EnableAllAction();
+    }
+
+    private void OnDisable()
+    {
+        DisableAllAction();
+    }
+
+    private void OnDestroy()
+    {
+        UnBindAllEvent();
+    }
+
+    public void BindPlayerMoveEvent(Action<InputAction.CallbackContext> context)
+    {
+        if(context == null) { return; }
+
+        _bindedPlayerMoveEvent = context;
+        PlayerMoveAction.action.performed += _bindedPlayerMoveEvent;
+    }
+
+    public void UnBindPlayerMoveEvent()
+    {
+        if(_bindedPlayerMoveEvent == null) { return; }
+
+        PlayerMoveAction.action.performed -= _bindedPlayerMoveEvent;
+    }
+
+    private void EnablePlayerMoveAction()
+    {
+        if (!PlayerMoveAction.action.enabled)
+        {
+            PlayerMoveAction.action.Enable();
+        }
+    }
+
+    private void DisablePlayerMoveAction()
+    {
+        if (PlayerMoveAction.action.enabled)
+        {
+            PlayerMoveAction.action.Disable();
+        }
+    }
+
+    private void EnableAllAction()
+    {
+        EnablePlayerMoveAction();
+    }
+
+    private void DisableAllAction()
+    {
+        DisablePlayerMoveAction();
+    }
+
+    private void UnBindAllEvent()
+    {
+        UnBindPlayerMoveEvent();
+    }
+}
+
