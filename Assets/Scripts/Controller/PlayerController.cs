f3e7b61562367d123179a68847fb71863ba70511
Jinkus <gdjinkus@gmail.com>
2026-05-20 13:22:01 +0900

    feat: [플레이어 이동] 플레이어 컨트롤러 스크립트 추가

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof( Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D _rigidbody;
+
+    [Header("Move")]
+    [SerializeField] private float _moveSpeed = 5.0f;
+   
+    private const float _moveInputDeadZone = 0.25f;
+
+    private Vector2 _moveDirection;
+  
+    private void OnEnable()
+    {
+        InputManager.Instance.BindPlayerMoveCallback(OnMoveInput, InputCallbackType.Performed);
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.UnbindPlayerMoveCallback();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnMoveInput(InputAction.CallbackContext context)
+    {
+        Vector2 moveInput = context.ReadValue<Vector2>();
+
+        _moveDirection = (moveInput.sqrMagnitude > _moveInputDeadZone) ? moveInput : Vector2.zero;
+    }
+
+    private void Move()
+    {
+        _rigidbody.linearVelocity = _moveDirection * _moveSpeed;
+    }
+}
+
+
+
