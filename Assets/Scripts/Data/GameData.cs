2fa4084677d610a6f120755c49016c0577699e37
Jinkus <gdjinkus@gmail.com>
2026-05-21 16:34:13 +0900

    Data: [데이터] 순수 데이터 클래스 추가

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class JsonWrapper<T>
+{
+    public List<T> items;
+}
+
+[Serializable]
+public abstract class GameData
+{
+    public string Id;
+}
---
+++ Assets/Scripts/Data/MonsterData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class MonsterData : GameData
+{
+    public string Name;
+    public string Description;
+}
