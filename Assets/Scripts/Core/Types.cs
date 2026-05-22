213f8b4d8ed0185e21772cf87c19003f3155c4ce
Jinkus <gdjinkus@gmail.com>
2026-05-22 18:27:58 +0900

    feat: [풀 매니저] 풀링 결과 반환용 구조체 추가

---
+++ Assets/Scripts/Core/Types.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct PoolResult
+{
+    public bool IsSuccess { get;  private set; }
+    public GameObject ResultObject { get; private set; }
+
+    public PoolResult(bool isSuccess, GameObject resultObject)
+    {
+        IsSuccess = isSuccess;
+        ResultObject = resultObject;
+    }
+}
