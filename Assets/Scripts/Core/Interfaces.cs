997bdfd979d9b247af18887c55201659b5362a69
Jinkus <gdjinkus@gmail.com>
2026-05-22 18:06:34 +0900

    feat: [인터페이스] 오브젝트 생성 및 풀링을 위한 기반 인터페이스 추가

---
+++ Assets/Scripts/Core/Interfaces.cs
@@ -0,0 +1,11 @@
+﻿public interface ISpawnableObject
+{
+    int InstanceId { get; }
+
+    void SetInstanceId(int instanceId);
+}
+
+public interface IPoolableObject : ISpawnableObject
+{
+    bool IsActive { get; }
+}
