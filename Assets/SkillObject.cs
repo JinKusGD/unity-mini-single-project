7aab138ccd23cde4f95ff9bb5d738b6908a75fb5
Jinkus <gdjinkus@gmail.com>
2026-05-28 23:20:41 +0900

    feat: [스킬] 스킬 오브젝트의 베이스 추상 클래스 스크립트 추가

---
+++ Assets/SkillObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SkillObject : MonoBehaviour, IPoolableObject
+{
+    public int InstanceId { get; private set; }
+
+    public bool IsActive { get; private set; }
+
+    protected virtual void OnEnable()
+    {
+        IsActive = true;
+    }
+
+    protected virtual void OnDisable()
+    {
+        IsActive = false;
+    }
+
+    public void SetInstanceId(int instanceId)
+    {
+        InstanceId = instanceId;
+    }
+}
