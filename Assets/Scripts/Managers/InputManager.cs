4bdbe74ad2baebe89098ab711bdfd450354997eb
Jinkus <gdjinkus@gmail.com>
2026-05-30 22:21:05 +0900

    refactor: [인풋 매니저] 경고/에러 로그 추가

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -23,6 +23,7 @@ public class InputManager : MonoBehaviour
     {
         if (Instance != null && Instance != this)
         {
+            Debug.LogWarning($"[{gameObject.name}] InputManager 인스턴스가 존재하여 기존 오브젝트를 파괴했습니다.");
             Destroy(gameObject);
             return;
         }
@@ -49,12 +50,16 @@ public class InputManager : MonoBehaviour
 
     public void BindPlayerMoveCallback(Action<InputAction.CallbackContext> callback, InputCallbackType callbackType)
     {
-        if (callback == null) { return; }
+        if (callback == null)
+        {
+            Debug.LogWarning($"[{InputActionType.PlayerMove}] 콜백 함수가 비어있어 바인딩을 진행하지 못했습니다.");
+            return;
+        }
 
         InputActionType inputActionType = InputActionType.PlayerMove;
         BindInputAction(callback, callbackType, inputActionType);
     }
-    
+
     public void UnbindPlayerMoveCallback()
     {
         InputActionType inputActionType = InputActionType.PlayerMove;
@@ -63,7 +68,11 @@ public class InputManager : MonoBehaviour
 
     public void BindPlayerDashCallback(Action<InputAction.CallbackContext> callback, InputCallbackType callbackType)
     {
-        if (callback == null) { return; }
+        if (callback == null)
+        {
+            Debug.LogWarning($"[{InputActionType.PlayerDash}] 콜백 함수가 비어있어 바인딩을 진행하지 못했습니다.");
+            return;
+        }
 
         InputActionType inputActionType = InputActionType.PlayerDash;
         BindInputAction(callback, callbackType, inputActionType);
@@ -85,6 +94,7 @@ public class InputManager : MonoBehaviour
             case InputActionType.PlayerDash:
                 return _playerDashBindingInfo;
             default:
+                Debug.LogError($"[{inputActionType}] 정의되지 않은 InputActionType 으로 InputBindingInfo를 찾지 못했습니다.");
                 break;
         }
 
@@ -100,6 +110,7 @@ public class InputManager : MonoBehaviour
             case InputActionType.PlayerDash:
                 return _playerDashAction;
             default:
+                Debug.LogError($"[{inputActionType}] 정의되지 않은 InputActionType 으로 InputActionReference를 찾지 못했습니다.");
                 break;
         }
 
@@ -109,9 +120,13 @@ public class InputManager : MonoBehaviour
     private void EnablePlayerMoveAction()
     {
         InputActionReference inputActionReference = GetInputActionReference(InputActionType.PlayerMove);
-        
-        if (inputActionReference == null) { return; }
-        
+
+        if (inputActionReference == null)
+        {
+            Debug.LogError($"[{InputActionType.PlayerMove}] InputActionReference가 없어 액션을 활성화하지 못했습니다.");
+            return;
+        }
+
         EnableAction(inputActionReference);
     }
 
@@ -119,7 +134,11 @@ public class InputManager : MonoBehaviour
     {
         InputActionReference inputActionReference = GetInputActionReference(InputActionType.PlayerMove);
 
-        if (inputActionReference == null) { return; }
+        if (inputActionReference == null)
+        {
+            Debug.LogError($"[{InputActionType.PlayerMove}] InputActionReference가 없어 액션을 비활성화하지 못했습니다.");
+            return;
+        }
 
         DisableAction(inputActionReference);
     }
@@ -127,9 +146,13 @@ public class InputManager : MonoBehaviour
     private void EnablePlayerDashAction()
     {
         InputActionReference inputActionReference = GetInputActionReference(InputActionType.PlayerDash);
-        
-        if (inputActionReference == null) { return; }
-        
+
+        if (inputActionReference == null)
+        {
+            Debug.LogError($"[{InputActionType.PlayerDash}] InputActionReference가 없어 액션을 활성화하지 못했습니다.");
+            return;
+        }
+
         EnableAction(inputActionReference);
     }
 
@@ -137,7 +160,11 @@ public class InputManager : MonoBehaviour
     {
         InputActionReference inputActionReference = GetInputActionReference(InputActionType.PlayerDash);
 
-        if (inputActionReference == null) { return; }
+        if (inputActionReference == null)
+        {
+            Debug.LogError($"[{InputActionType.PlayerDash}] InputActionReference가 없어 액션을 비활성화하지 못했습니다.");
+            return;
+        }
 
         DisableAction(inputActionReference);
     }
@@ -163,10 +190,14 @@ public class InputManager : MonoBehaviour
     #endregion
 
     #region 주요 로직(건드리지 말아주세요)
-   
+
     private void SetupInputBindingInfo(ref InputBindingInfo inputBindingInfo, Action<InputAction.CallbackContext> callback, InputCallbackType callbackType)
     {
-        if (inputBindingInfo == null) { return; }
+        if (inputBindingInfo == null)
+        {
+            Debug.LogError($"[바인딩 정보 세팅] nputBindingInfo가 없어 세팅하지 못했습니다.");
+            return;
+        }
 
         inputBindingInfo.CallbackType = callbackType;
         inputBindingInfo.Callback = callback;
@@ -174,10 +205,14 @@ public class InputManager : MonoBehaviour
 
     private void BindInputCallback(InputActionReference inputActionReference, InputBindingInfo inputBindingInfo)
     {
-        if (inputActionReference == null || inputActionReference.action == null || inputBindingInfo == null || inputBindingInfo.Callback == null) { return; }
+        if (inputActionReference == null || inputActionReference.action == null || inputBindingInfo == null || inputBindingInfo.Callback == null)
+        {
+            Debug.LogWarning($"[인풋 콜백 등록] 참조 요소를 확인하지 못해 콜백 등록에 실패했습니다.");
+            return;
+        }
 
         UnbindInputCallback(inputActionReference, inputBindingInfo);
-     
+
         switch (inputBindingInfo.CallbackType)
         {
             case InputCallbackType.Started:
@@ -190,13 +225,18 @@ public class InputManager : MonoBehaviour
                 inputActionReference.action.canceled += inputBindingInfo.Callback;
                 break;
             default:
+                Debug.LogError($"[{inputActionReference.action.name}] 정의되지 않은 InputCallbackType으로 콜백 등록에 실패했습니다.");
                 break;
         }
     }
 
     private void UnbindInputCallback(InputActionReference inputActionReference, InputBindingInfo inputBindingInfo)
     {
-        if (inputActionReference == null || inputActionReference.action == null || inputBindingInfo == null || inputBindingInfo.Callback == null) { return; }
+        if (inputActionReference == null || inputActionReference.action == null || inputBindingInfo == null || inputBindingInfo.Callback == null)
+        {
+            Debug.LogWarning($"[인풋 콜백 해제] 참조 요소를 확인하지 못해 콜백 해제에 실패했습니다.");
+            return;
+        }
 
         switch (inputBindingInfo.CallbackType)
         {
@@ -210,18 +250,27 @@ public class InputManager : MonoBehaviour
                 inputActionReference.action.canceled -= inputBindingInfo.Callback;
                 break;
             default:
+                Debug.LogError($"[{inputActionReference.action.name}] 정의되지 않은 InputCallbackType으로 콜백 해제에 실패했습니다.");
                 break;
         }
     }
-    
+
     private void BindInputAction(Action<InputAction.CallbackContext> callback, InputCallbackType callbackType, InputActionType inputActionType)
     {
-        if (callback == null || callbackType == InputCallbackType.None) { return; }
+        if (callback == null || callbackType == InputCallbackType.None)
+        {
+            Debug.LogWarning($"[{inputActionType}] 올바르지 않은 콜백 또는 None 타입으로 액션 등록을 실패했습니다.");
+            return;
+        }
 
         InputBindingInfo inputBindingInfo = GetInputBindingInfo(inputActionType);
         InputActionReference inputActionReference = GetInputActionReference(inputActionType);
 
-        if (inputBindingInfo == null || inputActionReference == null || inputActionReference.action == null) { return; }
+        if (inputBindingInfo == null || inputActionReference == null || inputActionReference.action == null)
+        {
+            Debug.LogError($"[{inputActionType}] InputBindingInfo 또는 InputActionReference가 유효하지 않아 액션 등록에 실패했습니다.");
+            return;
+        }
 
         SetupInputBindingInfo(ref inputBindingInfo, callback, callbackType);
         BindInputCallback(inputActionReference, inputBindingInfo);
@@ -232,15 +281,23 @@ public class InputManager : MonoBehaviour
         InputBindingInfo inputBindingInfo = GetInputBindingInfo(inputActionType);
         InputActionReference inputActionReference = GetInputActionReference(inputActionType);
 
-        if (inputBindingInfo == null || inputActionReference == null || inputActionReference.action == null) { return; }
+        if (inputBindingInfo == null || inputActionReference == null || inputActionReference.action == null)
+        {
+            Debug.LogError($"[{inputActionType}] InputBindingInfo 또는 InputActionReference가 유효하지 않아 액션 해제에 실패했습니다.");
+            return;
+        }
 
-        UnbindInputCallback(inputActionReference, inputBindingInfo);    
+        UnbindInputCallback(inputActionReference, inputBindingInfo);
         SetupInputBindingInfo(ref inputBindingInfo, null, InputCallbackType.None);
     }
 
     private void EnableAction(InputActionReference inputActionReference)
     {
-        if (inputActionReference == null || inputActionReference.action == null) { return; }
+        if (inputActionReference == null || inputActionReference.action == null)
+        {
+            Debug.LogError($"[액션 활성화] 인풋 액션 또는 내부 이벤트가 비어있어 액션을 활성화하지 못했습니다.");
+            return;
+        }
 
         if (!inputActionReference.action.enabled)
         {
@@ -250,13 +307,17 @@ public class InputManager : MonoBehaviour
 
     private void DisableAction(InputActionReference inputActionReference)
     {
-        if (inputActionReference == null || inputActionReference.action == null) { return; }
+        if (inputActionReference == null || inputActionReference.action == null)
+        {
+            Debug.LogError($"[액션 비활성화] 인풋 액션 또는 내부 이벤트가 비어있어 액션을 비활성화하지 못했습니다.");
+            return;
+        }
 
         if (inputActionReference.action.enabled)
         {
             inputActionReference.action.Disable();
         }
     }
-   
+
     #endregion
 }
