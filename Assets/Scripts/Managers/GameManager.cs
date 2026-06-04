4382aae55219e6394c0d91ce425e8aa24fe8d20e
Jinkus <gdjinkus@gmail.com>
2026-06-04 18:40:35 +0900

    feat: [게임 매니저] 게임 시작 로직을 추가한 게임 매니저 추가

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning($"[{gameObject.name}] GameManager 인스턴스가 존재하여 기존 오브젝트를 파괴했습니다.");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public async UniTask StartGame()
+    {
+        await ObjectManager.Instance.SpawnPlayerAsync("Player_001_Sylvia", Vector3.zero);
+        await UIManager.Instance.OpenDamageTextHudAsync();
+        UIManager.Instance.CloseTitleUI();
+        MapManager.Instance.StartSpawnLoop();
+    }
+}
