caed8d96c9127014262fea686d06437c96afadee
Jinkus <gdjinkus@gmail.com>
2026-06-08 18:27:08 +0900

    feat: [카메라 매니저] 카메라 컨트롤러를 관리하는 카메라 매니저 추가

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance { get; private set; }
+
+    [SerializeField] private CameraController _playerFollowCameraController;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning($"[{gameObject.name}] 이미 CameraManager 인스턴스가 존재하여 생성된 오브젝트를 파괴합니다.");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void SetPlayerTarget(Transform playerTransform)
+    {
+        if (playerTransform == null)
+        {
+            Debug.LogWarning($"[{nameof(CameraManager)}] SetPlayerTarget에 전달된 playerTransform가 null입니다.");
+            return;
+        }
+
+        _playerFollowCameraController.SetTrackingTarget(playerTransform);
+    }
+}
