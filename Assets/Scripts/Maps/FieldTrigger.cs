48627c68168dabdb9943e0ba83f477508e87cfac
이진규 <gdjinkus@gmail.com>
2026-07-28 17:41:59 +0900

    feat: [Map] 맵 이동 시 트리거 로직 추가

---
+++ Assets/Scripts/Maps/FieldTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FieldTrigger : MonoBehaviour
+{
+    [SerializeField] private string _fieldName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) { return; }
+
+        MapManager.Instance.ChangedField(_fieldName);
+    }
+}
